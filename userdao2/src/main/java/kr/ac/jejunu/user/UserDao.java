274981beec44f5427c91e57f477612dfb4291546
dhrgodms <dhrgodms123@gmail.com>
2023-04-07 18:43:48 +0900

    230407 refactoring 수업

--- userdao2/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userdao2/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -4,53 +4,30 @@ import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    private final DataSource dataSource;
+    private final JdbcContext jdbcContext;
 
-    public UserDao(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public UserDao(JdbcContext jdbcContext) {
+        this.jdbcContext = jdbcContext;
     }
 
     public User findById(Long id) throws ClassNotFoundException, SQLException {
-        //데이터 어딨어? mysql
-        //mysql 클래스 로딩
-        Connection connection = dataSource.getConnection();
-        //쿼리 만들고
-        PreparedStatement preparedStatement = connection.prepareStatement
-                ("select id, name, password from userinfo where id = ?");
-        preparedStatement.setLong(1, id);
-        //쿼리 실행하고
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-        //결과를 사용자 정보에 매핑하고
-        User user = new User();
-        user.setId(resultSet.getLong("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
-        //자원해지
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
-        //결과리턴
-
-        return user;
+        StatementStrategy statementStrategy = new FindStatementStrategy(id);
+        return jdbcContext.jdbcContextForFind(statementStrategy);
     }
 
     public void insert(User user) throws ClassNotFoundException, SQLException {
-        Connection connection = dataSource.getConnection();
-        //쿼리 만들고
-        PreparedStatement preparedStatement = connection.prepareStatement
-                ("insert into userinfo (name, password) values ( ?, ? )"
-                        , Statement.RETURN_GENERATED_KEYS);
-        preparedStatement.setString(1, user.getName());
-        preparedStatement.setString(2, user.getPassword());
-        //쿼리 실행하고
-        preparedStatement.executeUpdate();
-        ResultSet resultSet = preparedStatement.getGeneratedKeys();
-        resultSet.next();
-        user.setId(resultSet.getLong(1));
-        //자원해지
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+        StatementStrategy statementStrategy = new InsertStatementStrategy(user);
+        jdbcContext.jdbcContextForInsert(user, statementStrategy);
     }
+
+    public void update(User user) throws SQLException {
+        StatementStrategy statementStrategy = new UpdateStatementStrategy(user);
+        jdbcContext.jdbcContextForUpdate(statementStrategy);
+    }
+
+    public void delete(Long id) throws SQLException {
+        StatementStrategy statementStrategy = new DeleteStatementStrategy(id);
+        jdbcContext.jdbcContextForUpdate(statementStrategy);
+    }
+
 }
